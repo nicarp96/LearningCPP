f2cbfd92723e8b1eaf47536329d728e88fd3af85
nicarp <leoyotube0@gmail.com>
2024-02-27 18:25:29 +0200

    Expresii valide

--- Problems/expresii_valide/main.cpp
+++ Problems/expresii_valide/main.cpp
@@ -1,64 +1,61 @@
 #include <iostream>
 #include <fstream>
-#include <vector>
-#include <string>
-#include <algorithm>
+#include <stack>
 using namespace std;
 
-bool verifyCorectitude(string str){
-    size_t n;
-    string brackets[] {"[]","()","{}"};
+int acol=0, sqrbr=0, roundbr=0;
 
-    for(int i = 0;i<sizeof(brackets);i++){
-                n=str.find(brackets[i]);
-                if(n!=string::npos){
-                    str.erase(n);
-                    str.erase(n);
-
-                    if(str.length()==0){
-                        return true;
-                    }
-                    return verifyCorectitude(str);
-                }
+bool isBalanced(string str){
+    stack<char> temp;
+    bool answer = false;
+    for (int i = 0; i < str.length(); i++)
+    {
+        char c = str[i];
+        if(c=='('){
+            temp.push(')');
+            roundbr++;
+        }
+        else if(c=='['){
+            temp.push(']');
+            sqrbr++;
+        }
+        else if(c=='{'){
+            temp.push('}');
+            acol++;
+        }
+        else if (c==temp.top())
+        {
+            temp.pop();
+        }
     }
-    return false;
+    if(temp.empty()){
+        answer = true;
+    }
+    return answer;
 }
 
-struct brpair{
-    int opening;
-    int closing;
-};
 
-int main() {
+    int main()
+{
     ifstream fin("expresii.in");
-    int n;
-    fin>>n;
-    vector<string> strs;
-    for(int i = 0;i<n;i++){
-        string s;
-        fin>>s;
-        strs.push_back(s);
-    }
-    fin.close();
-    brpair sqrbr,roundbr,acol;
     ofstream fout("expresii.out");
-    for(auto x:strs){
-        sqrbr.opening=count(x.begin(),x.end(),'[');
-        sqrbr.closing=count(x.begin(),x.end(),']');
-        roundbr.opening=count(x.begin(),x.end(),'(');
-        roundbr.closing=count(x.begin(),x.end(),')');
-        acol.opening=count(x.begin(),x.end(),'{');
-        acol.closing=count(x.begin(),x.end(),'}');
-        if(sqrbr.opening==sqrbr.closing&&roundbr.closing==roundbr.opening&&acol.opening==acol.closing){
-            if(verifyCorectitude(x)){
-            fout<<"DA"<<" ("<<roundbr.opening<<") ["<<sqrbr.opening<<"] {"<<acol.opening<<"}\n";
-            }
+    int n;
+    fin >> n;
+    stack<char> temp;
+    for (int i = 0; i < n; i++)
+    {
+        acol = 0;
+        sqrbr = 0;
+        roundbr = 0;
+        string str;
+        fin >> str;
+        if(isBalanced(str)){
+            fout << "DA (" << roundbr << ") [" << sqrbr << "] {" << acol << "}" << endl;
         }
         else{
-            fout<<"NU\n";
+            fout << "NU" << endl;
         }
-
     }
+    fin.close();
     fout.close();
-    return 0;
 }
